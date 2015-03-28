5bc15bf8a8c45f1e235cc8b2f2e88e51996f8f3a
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2015-03-28 14:46:10 +0700

    Added inventory

---
+++ Assets/Scripts/Inventory/ItemDatabase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+	public List<Item> items = new List<Item>();
+
+	void Start() {
+		items.Add(new Item("Batu", 0, "Sebuah benda padat, bisa dipijaki", 0, 0, Item.ItemType.Weapon));
+		items.Add (new Item("Palu", 1, "Sebuah kakas", 0, 0, Item.ItemType.Weapon));
+		items.Add (new Item("Apel", 2, "Sebuah buah-buahan bervitamin A", 0, 0, Item.ItemType.Consumable));
+	}
+}
