237a67b9f9dd3350d792d29409f9630cbacfba32
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2015-03-29 18:19:34 +0700

    Use bomb, tapi belum ada timernya

---
+++ Assets/Stealth/Scripts/Inventory/StoneCrash.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneCrash : MonoBehaviour {
+
+	public AudioClip keyGrab;                       // Audioclip to play when the key is picked up.
+	
+	
+	private GameObject player;                      // Reference to the player.
+	//private PlayerInventory playerInventory;        // Reference to the player's inventory.
+	//private Inventory inventory;
+	public Inventory inventory;
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		player = GameObject.FindWithTag("Player");
+		
+	}
+
+	public IEnumerator setBomb()
+	{
+		yield return new WaitForSeconds (5);
+		Destroy (gameObject);
+		inventory.AddItem (1);
+		//AudioSource.PlayClipAtPoint(keyGrab, transform.position);
+	}
+
+}
---
+++ Assets/Stealth/Scripts/Player/UseBomb.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseBomb : MonoBehaviour {
+
+	private GameObject stone;
+	//public Transform bomb;
+	public Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+		stone = GameObject.FindWithTag("Stone");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("UseBomb"))
+		{
+			inventory.RemoveItem(0);
+			//var bombb = Instantiate(bomb, transform.position, transform.rotation);
+			//transform.localScale = new Vector3(0.01F, 0.01F, 0.01F);
+			StartCoroutine(placeBomb());
+			Destroy(stone);
+			inventory.AddItem(1);
+		}
+	}
+
+	IEnumerator placeBomb()
+	{
+		yield return new WaitForSeconds (100);
+	}
+}
