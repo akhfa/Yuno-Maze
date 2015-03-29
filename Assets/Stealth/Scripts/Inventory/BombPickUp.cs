69d4c388d0339d8cd65d0552f9bd9c777d32df96
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2015-03-29 17:08:31 +0700

    Added bomb to inventory

---
+++ Assets/Stealth/Scripts/Inventory/BombPickUp.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombPickUp : MonoBehaviour {
+
+	public AudioClip keyGrab;                       // Audioclip to play when the key is picked up.
+	
+	
+	private GameObject player;                      // Reference to the player.
+	//private PlayerInventory playerInventory;        // Reference to the player's inventory.
+	//private Inventory inventory;
+	public Inventory inventory;
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		player = GameObject.FindWithTag("Player");
+		
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// If the colliding gameobject is the player...
+		if(other.gameObject == player)
+		{
+			// ... play the clip at the position of the key...
+			AudioSource.PlayClipAtPoint(keyGrab, transform.position);
+			
+			// ... the player has a key ...
+			inventory.AddItem (0);
+			
+			// ... and destroy this gameobject.
+			Destroy(gameObject);
+		}
+	}
+}
