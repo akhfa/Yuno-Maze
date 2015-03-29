5815f9164aed32ad42cd77a8427caee455d17a0e
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2015-03-29 20:49:01 +0700

    Added script to detect player pos

---
+++ Assets/Stealth/Scripts/Player/PlayerPosition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPosition : MonoBehaviour {
+
+	private GameObject player;
+	Vector3 playerPos;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPos = player.transform.position ();
+		if (playerPos.y < 0)
+			Application.LoadLevel ("Level_1");
+	}
+}
